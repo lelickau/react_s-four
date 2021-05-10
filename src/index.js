9625b50c974f39d5c9f1cfaf7226672ee0a6f0bc
elena_kau <lelickau@yandex.ru>
2021-05-10 15:19:57 +0200

    1.1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import {createStore} from 'redux';
 
-//const initialState = 0;
-
 const reducer = (state = 0, action) => {
 	switch (action.type) {
 		case 'INC':
@@ -10,22 +8,17 @@ const reducer = (state = 0, action) => {
 		case 'DEC':
 			return state -1;
 
-		case 'RND':
-			return state + action.value;
+		case 'RES':
+			return 0;
 
 		default: 
 			return state;
 	}
 }
-//action creator incAC
+
 const incAC = () => ({type: 'INC'});
 const decAC = () => ({type: 'DEC'});
-const rndAC = (value) => ({type: 'RND', value})
-// const incAC = () => {
-// 	return {
-// 		type: 'INC'
-// 	}
-// }
+const resAC = () => ({type: 'RES'})
 
 const store = createStore(reducer);
 
@@ -37,9 +30,8 @@ document.getElementById('dec').addEventListener('click', () => {
 	store.dispatch(decAC());
 });
 
-document.getElementById('rnd').addEventListener('click', () => {
-	const value = Math.floor(Math.random() * 10);
-	store.dispatch(rndAC(value));
+document.getElementById('res').addEventListener('click', () => {
+	store.dispatch(resAC());
 });
 
 const updateStore = () => {
@@ -47,14 +39,4 @@ const updateStore = () => {
 }
 
 store.subscribe(updateStore);
-
-// store.subscribe(() => {
-// 	console.log(store.getState());
-// })
-
-// store.dispatch({type: 'INC'});
-
-// store.dispatch({type: 'INC'});
-
-// store.dispatch({type: 'INC'});
 
