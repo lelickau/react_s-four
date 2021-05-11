15e3dec5a7d69d5af7f99c2598e95d7c4e5d80dc
elena_kau <lelickau@yandex.ru>
2021-05-11 15:03:56 +0200

    1.2.4

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
 import * as actions from '../actions';
 
 const Counter = ({counter, inc, dec, res, rnd}) => {
@@ -31,7 +30,5 @@ const mapStateToProps = (state) => {
         counter: state
     }
 }
-const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators(actions, dispatch);
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+export default connect(mapStateToProps, actions)(Counter);
