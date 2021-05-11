4d99e3c15dfda3f05b50866a72bc61a490c7305b
elena_kau <lelickau@yandex.ru>
2021-05-11 13:31:48 +0200

    1.2

---
+++ src/actions.js
@@ -0,0 +1,4 @@
+export const incAC = () => ({type: 'INC'});
+export const decAC = () => ({type: 'DEC'});
+export const resAC = () => ({type: 'RES'});
+export const rndAC = (value) => ({type: 'RND', value});
--- src/index.js
+++ src/index.js
@@ -1,38 +1,32 @@
-import {createStore} from 'redux';
-
-const reducer = (state = 0, action) => {
-	switch (action.type) {
-		case 'INC':
-			return state +1;
-		
-		case 'DEC':
-			return state -1;
-
-		case 'RES':
-			return 0;
-
-		default: 
-			return state;
-	}
-}
-
-const incAC = () => ({type: 'INC'});
-const decAC = () => ({type: 'DEC'});
-const resAC = () => ({type: 'RES'})
+import {createStore, bindActionCreators} from 'redux';
+import reducer from './reducer';
+import {incAC, decAC, resAC, rndAC} from './actions';
 
 const store = createStore(reducer);
+const {dispatch} = store;
+const {incDispatch, decDispatch, resDispatch, rndDispatch} = bindActionCreators(
+	{
+		incDispatch: incAC,
+		decDispatch: decAC,
+		resDispatch: resAC,
+		rndDispatch: rndAC,
+	}
+	, dispatch);
 
-document.getElementById('inc').addEventListener('click', () => {
-	store.dispatch(incAC());
-});
+// const decDispatch = bindActionCreators(decAC, dispatch);
+// const resDispatch = bindActionCreators(resAC, dispatch);
+// const rndDispatch = bindActionCreators(rndAC, dispatch);
 
-document.getElementById('dec').addEventListener('click', () => {
-	store.dispatch(decAC());
-});
+document.getElementById('inc').addEventListener('click', incDispatch);
 
-document.getElementById('res').addEventListener('click', () => {
-	store.dispatch(resAC());
-});
+document.getElementById('dec').addEventListener('click', decDispatch);
+
+document.getElementById('res').addEventListener('click', resDispatch);
+
+document.getElementById('rnd').addEventListener('click', () => {
+	const value = Math.floor(Math.random() * 10);
+	rndDispatch(value)
+})
 
 const updateStore = () => {
 	document.getElementById('counter').textContent = store.getState();
