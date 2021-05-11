a38ecc0b72b2264699fe1b4c3b33a767fbb7fbd1
elena_kau <lelickau@yandex.ru>
2021-05-11 14:14:13 +0200

    1.2.1

---
+++ src/counter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Counter = ({counter, inc, dec, res, rnd}) => {
+    return (
+        <div className="container">
+            <div className="item">
+                <div className="counter">{counter}</div>
+            </div>
+            <div className="btn-box">
+                <button onClick={dec} className="btn btn-minus">
+                    <img src={process.env.PUBLIC_URL + 'img/Minus.svg'} alt="minus" />
+                </button>
+                <button onClick={res} className="btn btn-reset">
+                    <img src={process.env.PUBLIC_URL + "img/reset.svg"} alt="reset" />
+                </button>
+                <button onClick={rnd} className="btn btn-reset">
+                    <img src={process.env.PUBLIC_URL + "img/reset.svg"} alt="random" />
+                </button>
+                <button onClick={inc} className="btn btn-plus">
+                    <img src={process.env.PUBLIC_URL + "img/plus.svg"} alt="plus" />
+                </button>
+            </div>
+        </div>
+    )
+}
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
 import * as actions from './actions';
+import Counter from './counter';
 
 const store = createStore(reducer);
 const {dispatch} = store;
 const {inc, dec, res, rnd} = bindActionCreators(actions, dispatch);
 
-document.getElementById('inc').addEventListener('click', inc);
+// document.getElementById('inc').addEventListener('click', inc);
 
-document.getElementById('dec').addEventListener('click', dec);
+// document.getElementById('dec').addEventListener('click', dec);
 
-document.getElementById('res').addEventListener('click', res);
+// document.getElementById('res').addEventListener('click', res);
 
-document.getElementById('rnd').addEventListener('click', () => {
-	const value = Math.floor(Math.random() * 10);
-	rnd(value)
-})
+// document.getElementById('rnd').addEventListener('click', () => {
+// 	const value = Math.floor(Math.random() * 10);
+// 	rnd(value)
+// });
 
 const updateStore = () => {
-	document.getElementById('counter').textContent = store.getState();
+	ReactDOM.render(<Counter  
+		counter={store.getState()}
+		inc={inc}
+		dec={dec}
+		res={res}
+		rnd={() => {
+			const value = Math.floor(Math.random() * 10);
+			rnd(value);
+		}}
+/>, document.getElementById('root'));
 }
+updateStore();
 
 store.subscribe(updateStore);
+
 
